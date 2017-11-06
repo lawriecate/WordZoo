9661228253c893e8612045efc42de40a38b5e960
Lawrie Cate <lawrie@catefamily.co.uk>
2017-11-06 00:48:13 +0000

    Updated basic login mechanism

--- Website/app.js
+++ Website/app.js
@@ -22,19 +22,15 @@ app.use(logger('dev'));
 
 var flash    = require('connect-flash');
 var crypto   = require('crypto');
-/* Login script */
-var passport = require('passport');
-var LocalStrategy  = require('passport-local').Strategy;
 var connection     = require('./lib/dbconn');
 
- var sess  = require('express-session');
- var Store = require('express-session').Store;
+var sess  = require('express-session');
+var Store = require('express-session').Store;
 var BetterMemoryStore = require('session-memory-store')(sess);
- var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
- app.use(sess({
+var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
+app.use(sess({
     name: 'JSESSION',
     secret: 'MYSECRETISVERYSECRET',
-    store:  store,
     resave: true,
     saveUninitialized: true
 }));
@@ -46,62 +42,48 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(flash());
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
 
-//passport Strategy -- the express session middleware before calling passport.session()
-passport.use('local', new LocalStrategy({
-  emailField: 'email',
-  passwordField: 'password',
-  passReqToCallback: true //passback entire req to call back
-} , function (req, email, password, done){
-      console.log(email+' = '+ password);
-      if(!email || !password ) { return done(null, false, req.flash('message','All fields are required.')); }
-      var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
-      connection.query("select * from users where email = ?", [email], function(err, rows){
-          console.log(err);
-        if (err) return done(req.flash('message',err));
 
-        if(!rows.length){ return done(null, false, req.flash('message','Invalid email or password.')); }
-        salt = salt+''+password;
-        var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
-        var dbPassword  = rows[0].password;
 
-        if(!(dbPassword == encPassword)){
-          console.log("Login failure");
-            return done(null, false, req.flash('message','Invalid email or password.'));
-         }
-
-        return done(null, rows[0]);
-      });
-    }
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    return done(null, false, { message: 'No connection.' });
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
 ));
 
-passport.serializeUser(function(user, done){
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done){
-    connection.query("select * from users where id = "+ id, function (err, rows){
-        done(err, rows[0]);
-    });
-});
-
 app.get('/signin', function(req, res){
-  res.render('login',{'message' :req.flash('message')});
+//  console.log(req.flash('error'));
+  res.render('login',{message :req.flash('error')});
 });
 
-app.post("/signin", passport.authenticate('local', {
-    successRedirect: '/profile',
-    failureRedirect: '/signin',
-    failureFlash: true
-}), function(req, res, info){
-  console.log('signinpos');
-    res.render('login',{'message' :req.flash('message')});
-});
+app.post("/signin",
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/signin',
+                                   failureFlash: true
+                                 })
+                      );
 
 app.get('/logout', function(req, res){
     req.session.destroy();
