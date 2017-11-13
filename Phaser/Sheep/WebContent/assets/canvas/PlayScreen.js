bbf56fcfbf21bc1222c01bd1ea9fe12fbcb3fa51
Oliver <32636766+obodinar@users.noreply.github.com>
2017-11-13 20:40:20 +0000

    Sheep - working
    Needs StartScreen, pic on Sheep, EndScreen

---
+++ Phaser/Sheep/WebContent/assets/canvas/PlayScreen.js
@@ -0,0 +1,464 @@
+// Global variables
+var sheep = sheep|| {}; 
+
+
+function PlayScreen() 
+{	
+	Phaser.State.call(this);
+}
+
+/** @type Phaser.State */
+var PlayScreen_proto = Object.create(Phaser.State.prototype);
+PlayScreen.prototype = PlayScreen_proto;
+PlayScreen.prototype.constructor = PlayScreen;
+
+PlayScreen.prototype.init = function () 
+{	
+	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	this.scale.pageAlignHorizontally = true;
+	this.scale.pageAlignVertically = true;
+	this.stage.backgroundColor = '#ffffff';	
+};
+
+PlayScreen.prototype.preload = function () 
+{	
+	this.load.pack('PlayScreen', 'assets/pack.json');
+
+	// Set up pens array + fill with blanks
+	pens = new Array();
+	for(var i = 0; i < 4; i++){
+		pens[i] = new Object();
+	}	
+};
+
+
+PlayScreen.prototype.create = function () 
+{	
+
+	// Add background
+	background = this.add.tileSprite(0, 0, 1920, 1080, 'Background');
+	background.scale.setTo(1.51, 1.51);
+
+
+	// Add back to pens
+	pens[0].back =  this.add.sprite(374, 387, 'penBack');
+	pens[0].back.anchor.setTo(0.5, 0.5);
+
+	pens[1].back = this.add.sprite(1581, 381, 'penBack');
+	pens[1].back.anchor.setTo(0.5, 0.5);
+	
+	pens[2].back = this.add.sprite(383, 871, 'penBack');
+	pens[2].back.anchor.setTo(0.5, 0.5);
+	
+	pens[3].back = this.add.sprite(1592, 868, 'penBack');
+	pens[3].back.anchor.setTo(0.5, 0.5);
+	
+
+	// Add sheep to pens
+	pens[0].sheep = this.add.sprite(68, 210, 'sheepPen', 12);	
+	pens[0].front = this.add.sprite(374, 387, 'penFront');
+	pens[0].front.anchor.setTo(0.5, 0.5);
+
+	pens[1].sheep = this.add.sprite(1273, 198, 'sheepPen', 12);
+	pens[1].front = this.add.sprite(1581, 381, 'penFront');
+	pens[1].front.anchor.setTo(0.5, 0.5);
+
+	pens[2].sheep = this.add.sprite(75, 697, 'sheepPen', 12);
+	pens[2].front = this.add.sprite(383, 871, 'penFront');
+	pens[2].front.anchor.setTo(0.5, 0.5);
+	
+	pens[3].sheep = this.add.sprite(1266, 709, 'sheepPen', 12);
+	pens[3].front = this.add.sprite(1592, 868, 'penFront');
+	pens[3].front.anchor.setTo(0.5, 0.5);
+
+
+	// Add GUI
+	this.add.sprite(0, 0, 'gui');
+
+
+	// Text over GUI
+	mainText = this.add.text(960, 90, "", bigStyle);
+	mainText.anchor.setTo(0.5, 0.5);
+
+	TRtext = this.add.text(1640, 150, "", medStyle);
+	TRtext.anchor.setTo(0.5, 0.5);
+		
+	TLtext = this.add.text(430, 165, "", medStyle);
+	TLtext.anchor.setTo(0.5, 0.5);
+
+	BRtext = this.add.text(1660, 650, "", medStyle);
+	BRtext.anchor.setTo(0.5, 0.5);
+
+	BLtext = this.add.text(450, 650, "", medStyle);
+	BLtext.anchor.setTo(0.5, 0.5);
+
+
+	// Add score value
+	scoreText = this.add.text(1650, 40, "Score: "+score, smallStyle);
+	scoreText.anchor.setTo(0.5, 0.5);
+
+	// Add time value
+	timeText = this.add.text(520, 40, "Time: "+time, smallStyle);
+	timeText.anchor.setTo(0.5, 0.5);
+
+	// Add Lives box
+	livesBox = this.add.sprite(0, 0, 'Lives', 0);
+
+
+	// Add sheep wobble animation
+	sheepWobble = this.add.sprite(948, 560, 'sheepWobble', 0);
+	sheepWobble.visible = false;
+	sheepWobble.anchor.setTo(0.5, 0.5);
+	sheepWobble.animations.add('wobble');
+	sheepWobble.scale.setTo(0.4,0.4);
+	sheepWobble.animations.play('wobble',30,true);
+
+	// Add main sheep
+	mainSheep = this.add.sprite(943, 527, 'mainSheep');
+	mainSheep.anchor.setTo(0.5, 0.5);
+	mainSheep.inputEnabled = true;
+	mainSheep.events.onInputDown.add(this.dragSheep);
+
+	
+	// Time
+	timeLeft = time;
+	timer = this.time.create(false);
+	timer.loop(Phaser.Timer.SECOND, this.updateTime, this);
+	timer.start();
+
+	// starting values
+	livesLeft = startingLives;
+	score = 0;
+
+
+	// Start game
+	this.spawnItems();		
+};
+
+
+
+// display current time to screen (with --)
+PlayScreen.prototype.updateTime = function ()
+{	
+	// update game
+	this.update();
+
+	// If no time remaining, game finished
+	if(timeLeft <= 0){
+		this.endGame();
+	}
+	
+	timeText.setText("Time: "+(--timeLeft), true);	
+};
+
+// display current score to screen
+PlayScreen.prototype.updateScore = function ()
+{	
+	scoreText.setText("Score: "+score, true);
+};
+
+
+
+// on dragSheet, switch sprites to wobble
+PlayScreen.prototype.dragSheep = function ()
+{
+	dragging = true;
+	sheepWobble.visible = true;
+	mainSheep.visible = false;
+}
+
+// on dropSheep, switch back back to static
+PlayScreen.prototype.dropSheep = function ()
+{
+	sheepWobble.position.setTo(948,560);
+	dragging = false;
+	sheepWobble.visible = false;
+	mainSheep.visible = true;
+
+
+	//Check where we dropped it, if it's correct, add a sheep
+	if(this.checkOverlap(sheepWobble,pens[0].sheep))
+	{
+		if(correctPen == 0)
+		{
+			this.addSheepToPen(0);
+
+			// Increase score
+			score++;
+			this.updateScore();
+
+			// reset
+			this.spawnItems();
+		}
+		else 
+		{
+			// Lose a life
+			livesLeft--;
+			this.checkLives();
+
+			// reset
+			this.spawnItems();
+		}
+		return;
+	} 
+	
+	if(this.checkOverlap(sheepWobble,pens[1].sheep))
+	{
+		if(correctPen == 1)
+		{
+			this.addSheepToPen(1);
+
+			// Increase score
+			score++;
+			this.updateScore();
+
+			// reset
+			this.spawnItems();
+		}
+		else 
+		{
+			// Lose a life
+			livesLeft--;
+			this.checkLives();
+
+			// reset
+			this.spawnItems();
+		}
+		return;
+	}
+	
+	if(this.checkOverlap(sheepWobble,pens[2].sheep))
+	{
+		if(correctPen == 2)
+		{
+			this.addSheepToPen(2);
+
+			// Increase score
+			score++;
+			this.updateScore();
+
+			// reset
+			this.spawnItems();
+		}
+		else 
+		{
+			// Lose a life
+			livesLeft--;
+			this.checkLives();
+
+			// reset
+			this.spawnItems();
+		}
+		return;
+	} 
+	
+	if(this.checkOverlap(sheepWobble,pens[3].sheep))
+	{
+		if(correctPen == 3)
+		{
+			this.addSheepToPen(3);
+
+			// Increase score
+			score++;
+			this.updateScore();
+
+			// reset
+			this.spawnItems();
+		}
+		else 
+		{
+			// Lose a life
+			livesLeft--;
+			this.checkLives();
+
+			// reset
+			this.spawnItems();
+		}
+		return;
+	} 
+}
+
+
+
+// enlarge pen on drag-over
+PlayScreen.prototype.expandPen = function (pen)
+{
+	pens[pen].back.scale.setTo(1.5,1.5);
+	pens[pen].front.scale.setTo(1.5,1.5);
+}
+
+// reset pen to normal size
+PlayScreen.prototype.resetPens = function (pen)
+{
+	for(var i = 0; i < 4; i++)
+	{
+		pens[i].back.scale.setTo(1,1);
+		pens[i].front.scale.setTo(1,1);
+	}
+}
+
+
+
+// check if spriteA is overlapping spriteB
+PlayScreen.prototype.checkOverlap = function (spriteA, spriteB)
+{
+    var boundsA = spriteA.getBounds();
+    var boundsB = spriteB.getBounds();
+    return Phaser.Rectangle.intersects(boundsA, boundsB);
+}
+
+// increase number of sheep in pen
+PlayScreen.prototype.addSheepToPen = function (pen)
+{
+	var temp = pens[pen].sheep.frame;
+	
+	temp--;
+	//Stop us going past the first frame
+	if(temp < 0){
+		temp = 0;
+	}
+	
+	pens[pen].sheep.frame = temp;
+}
+
+
+
+// on action, check to see if valid move
+PlayScreen.prototype.update = function(){
+	
+	//Reset All Pens
+	this.resetPens();
+
+	//Check if over any of the pens
+	if(dragging){
+		sheepWobble.position = this.input.position;
+		
+		if(this.checkOverlap(sheepWobble,pens[0].sheep))
+		{
+			this.expandPen(0);
+		}		
+		else if(this.checkOverlap(sheepWobble,pens[1].sheep))
+		{
+			this.expandPen(1);
+		}
+		else if(this.checkOverlap(sheepWobble,pens[2].sheep))
+		{
+			this.expandPen(2);
+		}
+		else if(this.checkOverlap(sheepWobble,pens[3].sheep))
+		{
+			this.expandPen(3);
+		} 		
+	}
+	
+	// Check if drag sheep has ended
+	if(this.input.activePointer.isUp)
+	{
+		if(dragging)
+		{
+			this.dropSheep();
+		}
+	}	
+};
+
+
+
+
+// Spawn items
+PlayScreen.prototype.spawnItems = function ()
+{	
+	// random index for correct pen
+	correctPen = Math.floor(Math.random() * 4);
+
+	// random indexs for words
+	correctRow = groupWords[Math.floor(Math.random() * groupWords.length)];
+
+
+	// Get random words to fill incorrect text boxes
+	incorrectWord1 = randomWords[Math.floor(Math.random() * randomWords.length)];
+	incorrectWord2 = randomWords[Math.floor(Math.random() * randomWords.length)];
+	incorrectWord3 = randomWords[Math.floor(Math.random() * randomWords.length)];
+
+	// While 2 are the same, reset util you get unique
+	while(incorrectWord1 == incorrectWord2 || incorrectWord1 == incorrectWord3 || incorrectWord2 == incorrectWord3)
+	{
+		incorrectWord1 = randomWords[Math.floor(Math.random() * randomWords.length)];
+		incorrectWord2 = randomWords[Math.floor(Math.random() * randomWords.length)];
+		incorrectWord3 = randomWords[Math.floor(Math.random() * randomWords.length)];
+	}
+
+
+	// Get a pair of matching values
+	correctWordA = correctRow[Math.floor(Math.random() * correctRow.length)];
+	correctWordB = correctRow[Math.floor(Math.random() * correctRow.length)];
+
+	// Make sure correctWordA isn't the same as correctWordB
+	while(correctWordA == correctWordB)
+	{
+		correctWordA = correctRow[Math.floor(Math.random() * correctRow.length)];
+		correctWordB = correctRow[Math.floor(Math.random() * correctRow.length)];
+	}
+
+	// Set one correct word to mainText
+	mainText.setText(correctWordA);
+
+
+	// Set words to screen
+	switch(correctPen){
+		case 0: correctPen = 0;
+				TLtext.setText(correctWordB);
+				TRtext.setText(incorrectWord1);
+				BLtext.setText(incorrectWord2);
+				BRtext.setText(incorrectWord3);
+				break;
+
+		case 1: correctLocation = 1;
+				TLtext.setText(incorrectWord1);
+				TRtext.setText(correctWordB);
+				BLtext.setText(incorrectWord2);
+				BRtext.setText(incorrectWord3);
+				break;
+		
+		case 2: correctLocation = 2;
+				TLtext.setText(incorrectWord1);
+				TRtext.setText(incorrectWord2);
+				BLtext.setText(correctWordB);
+				BRtext.setText(incorrectWord3);
+				break;
+				
+		case 3: correctLocation = 3;
+				TLtext.setText(incorrectWord1);
+				TRtext.setText(incorrectWord2);
+				BLtext.setText(incorrectWord3);
+				BRtext.setText(correctWordB);
+				break;
+	}
+}
+
+
+
+
+// Check how many lives remaining, show correct frame
+PlayScreen.prototype.checkLives = function() 
+{
+	if(livesLeft <= 0)
+	{
+		livesBox = this.add.sprite(0, 0, 'Lives', 3);
+		this.endGame();
+	}
+	else if(livesLeft == 1)
+	{
+		livesBox = this.add.sprite(0, 0, 'Lives', 2);
+	}
+	else if(livesLeft == 2)
+	{
+		livesBox = this.add.sprite(0, 0, 'Lives', 1);
+	}
+};
+
+
+// Game has finished, move to finish state
+PlayScreen.prototype.endGame = function() 
+{
+	this.state.start('finish');
+};
---
+++ Phaser/Sheep/WebContent/js/Main.js
@@ -0,0 +1,87 @@
+	//global variables
+	var sheep = sheep|| {}; 
+
+	// --------------------------------------------
+	// *** To be pulled from server not static ***
+	// --------------------------------------------
+
+
+
+	//word groups
+	var groupWords = 	[["A1","A2","A3","A4","A5","A6","A7","A8"],
+						["B1","B2","B3","B4","B5","B6","B7","B8"],
+						["C1","C2","C3","C4","C5","C6","C7","C8"],
+						["D1","D2","D3","D4","D5","D6","D7","D8"]];
+
+	//word bank
+	var randomWords = 	["Bench","Brain","Bread","Broccoli","Cage","Cannon",
+	            		"Castle","Chair","Clock","Crayon","Desk","Donkey",
+	            		"Feather","Fireman","Flag","Football","Grape","Guitar",
+	         			"Heart","Island","Jumper","Mouse","Notebook","Owl",
+	         			"Pear","Police","Rabbit","Rake","Soap","Squirrel"];
+
+
+	// sheep + onDrag animation
+	var mainSheep;
+	var sheepWobble;
+	var dragging = false;
+
+	// sheep pens
+	var pens;
+
+
+	// Correct values
+	var correctPen;
+	var correctWordA;
+	var correctWordB;
+	var correctRow;
+
+	// Incorrect values
+	var incorrectWord1;
+	var incorrectWord2;
+	var incorrectWord3;
+
+	// screen text
+	var mainText;
+	var TRtext;
+	var TLtext;
+	var BRtext;
+	var BLtext;
+
+	
+	// score
+	var score = 0;
+	var scoreText = "";
+	
+	// lives
+	var startingLives = 3;
+	var livesLeft;
+	var livesBox;
+	
+	// time
+	var time = 60;
+	var timeText = "";
+
+	// timer
+	var timer;
+	
+	// Text styles
+	var smallStyle = {font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+	var medStyle = {font: "bold 45px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+	var bigStyle = {font: "bold 100px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+	
+	
+window.onload = function() 
+{	
+	// Create game
+	var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+	
+	// Add states
+	game.state.add('start', StartScreen);
+	game.state.add('rules', RulesScreen);
+	game.state.add('play', PlayScreen);
+	game.state.add('finish', FinishScreen);
+	
+	// Start game
+	game.state.start('start');
+};
