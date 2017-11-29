046c6c1656eb1eb8d52aae0e21d058adb200c285
Oliver <32636766+obodinar@users.noreply.github.com>
2017-11-29 15:56:20 +0000

    Octopus - Tutorial Added

---
+++ Phaser/OctopusPizza/WebContent/assets/canvas/FinishScreen.js
@@ -0,0 +1,60 @@
+//global variables
+var octopus = octopus|| {}; 
+
+
+function FinishScreen() 
+{	
+	Phaser.State.call(this);	
+}
+
+/** @type Phaser.State */
+var FinishScreen_proto = Object.create(Phaser.State.prototype);
+FinishScreen.prototype = FinishScreen_proto;
+FinishScreen.prototype.constructor = FinishScreen;
+
+FinishScreen.prototype.init = function () 
+{
+	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	this.scale.pageAlignHorizontally = true;
+	this.scale.pageAlignVertically = true;
+	this.stage.backgroundColor = '#ffffff';
+};
+
+FinishScreen.prototype.preload = function () 
+{	
+	this.load.pack('FinishScreen', 'assets/pack.json');	
+};
+
+FinishScreen.prototype.create = function () 
+{
+	// Add background
+	var _FinishBackgroun = this.add.sprite(-1, -1, 'endBackGround');
+	_FinishBackgroun.scale.setTo(1.51, 1.51);
+
+	// *** Change text? ***
+	// Add 'unlucky text'
+	var _Unlucky = this.add.sprite(576, 96, 'Unlucky');
+	_Unlucky.scale.setTo(1.5, 1.5);	
+	
+	
+	// Play again button
+	var _PlayAgain = this.add.button(672, 384, 'playAgain', this.playAgain, this, null, null, null, null);
+	_PlayAgain.scale.setTo(1.5, 1.5);
+	
+	// Other games button
+	var _MoreGames = this.add.button(672, 576, 'moreGames', this.moreGames, this, null, null, null, null);
+	_MoreGames.scale.setTo(1.5, 1.5);
+};
+
+
+//on click playAgain, proceed to play screen
+FinishScreen.prototype.playAgain = function() 
+{
+	this.state.start('Start');
+};
+
+//on click moreGames, proceed to game selection page
+FinishScreen.prototype.moreGames = function() 
+{
+	console.log("Go to game selection page");
+};
---
+++ Phaser/OctopusPizza/WebContent/js/Main.js
@@ -0,0 +1,43 @@
+//global variables
+var octopus = octopus|| {}; 
+
+
+//Game assets
+var game;
+var assets;
+var dragItem;
+var dragging;
+var _pizza;
+
+//current order
+var order;
+var orderText;
+var strikes;
+var toppings;
+
+//Clock params
+var clock;
+var initialTime;
+var timeAllowed = 60;
+
+//Score
+var score = 0;
+var scoreText;
+
+var remainingItems;
+
+
+
+
+window.onload = function() 
+{
+	var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+
+	// Add the States your game has.
+	game.state.add("Start", StartScreen);
+	game.state.add("Rules", RulesScreen);
+	game.state.add("Level", Level);
+	game.state.add("Finish", FinishScreen);
+
+	game.state.start("Start");
+};
