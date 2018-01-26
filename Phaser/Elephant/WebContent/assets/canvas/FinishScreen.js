4dc7f7a59136e839836158d35eff40a69d263553
Oliver <32636766+obodinar@users.noreply.github.com>
2018-01-26 17:02:24 +0000

    Elephant - fixed finish page text

--- Phaser/Elephant/WebContent/assets/canvas/FinishScreen.js
+++ Phaser/Elephant/WebContent/assets/canvas/FinishScreen.js
@@ -39,7 +39,7 @@ FinishScreen.prototype.create = function ()
 	// Add score + score text
 	var _Score = this.add.sprite(32, 32, 'ScoreBackground');
 	_Score.scale.setTo(0.65, 1.5);
-	scoreText = this.add.text(90, 75, "Score: "+score, smallStyle);
+	scoreText = this.add.text(50, 60, "Score: "+score, medStyle);
 	scoreText.addColor('#FF9933', 0);
 	
 	// Play again button
