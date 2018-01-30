f166d20099248b5f9c3ec6df7e133795cddc0f55
Lawrie Cate <lawrie@catefamily.co.uk>
2018-01-30 13:21:29 +0000

    Updated sheep game

--- Website/assets/sheepgame/js/Main.js
+++ Website/assets/sheepgame/js/Main.js
@@ -1,114 +1,133 @@
-	//global variables
-	var sheep = sheep|| {};
+//global variables
+var sheep = sheep|| {};
 
-	// --------------------------------------------
-	// *** To be pulled from server not static ***
-	// --------------------------------------------
-
-
-
-	//word groups
-	var groupWords = 	[["A1","A2","A3","A4","A5","A6","A7","A8"],
-						["B1","B2","B3","B4","B5","B6","B7","B8"],
-						["C1","C2","C3","C4","C5","C6","C7","C8"],
-						["Cat","Hat","Mat","Vat","Pat","Bat","Fat"]];
-
-	//word bank
-	var randomWords = 	["Bench","Brain","Bread","Broccoli","Cage","Cannon",
-	            		"Castle","Chair","Clock","Crayon","Desk","Donkey",
-	            		"Feather","Fireman","Flag","Football","Grape","Guitar",
-	         			"Heart","Island","Jumper","Mouse","Notebook","Owl",
-	         			"Pear","Police","Rabbit","Rake","Soap","Squirrel"];
-
-	// Record word answers
-		// [(string) targetWord, (string/null) incorrectSelectedWord, (bool) pickedCorrectly, (int - ms) timeTaken]
-	var wordHistory = [];
-	var numWordHistory;
-	var startTime;
-
-	// Correct word vales
-	var TLraw;
-	var TRraw;
-	var BLraw;
-	var BRraw;
+// --------------------------------------------
+// *** To be pulled from server not static ***
+// --------------------------------------------
 
 
-	// Record click history
-		// [(int) x, (int) y, (UTCString) timeStamp]
-	var clickHistory = [];
-	var numClickHistory;
-	var gameStartTime;
+
+//word groups
+var groupWords = 	[["A1","A2","A3","A4","A5","A6","A7","A8"],
+					["B1","B2","B3","B4","B5","B6","B7","B8"],
+					["C1","C2","C3","C4","C5","C6","C7","C8"],
+					["Cat","Hat","Mat","Vat","Pat","Bat","Fat"]];
+
+//word bank
+var randomWords = 	["Bench","Brain","Bread","Broccoli","Cage","Cannon",
+								"Castle","Chair","Clock","Crayon","Desk","Donkey",
+								"Feather","Fireman","Flag","Football","Grape","Guitar",
+							"Heart","Island","Jumper","Mouse","Notebook","Owl",
+							"Pear","Police","Rabbit","Rake","Soap","Squirrel"];
+
+// Record word answers
+	// [(string) targetWord, (string/null) incorrectSelectedWord, (bool) pickedCorrectly, (int - ms) timeTaken]
+var wordHistory = [];
+var numWordHistory;
+var startTime;
+
+// Record click history
+	// [(int) x, (int) y, (UTCString) timeStamp]
+var clickHistory = [];
+var numClickHistory;
+var gameStartTime;
+
+// Player coins
+var startingCoins;
+var earnedCoins;
+var totalCoins;
 
 
-	// sheep + onDrag animation
-	var mainSheep;
-	var sheepWobble;
-	var dragging = false;
+// Correct word vales
+var TLraw;
+var TRraw;
+var BLraw;
+var BRraw;
 
-	// sheep pens
-	var pens;
+// sheep + onDrag animation
+var mainSheep;
+var sheepWobble;
+var dragging = false;
 
-
-	// Correct values
-	var correctPen;
-	var correctWordA;
-	var correctWordB;
-	var correctRow;
-
-	// Incorrect values
-	var incorrectWord1;
-	var incorrectWord2;
-	var incorrectWord3;
-
-	// screen text
-	var mainText;
-	var TRtext;
-	var TLtext;
-	var BRtext;
-	var BLtext;
+// sheep pens
+var pens;
 
 
-	// score
-	var score = 0;
-	var scoreText = "";
+// Correct values
+var correctPen;
+var correctWordA;
+var correctWordB;
+var correctRow;
 
-	// lives
-	var startingLives = 3;
-	var livesLeft;
-	var livesBox;
+// Incorrect values
+var incorrectWord1;
+var incorrectWord2;
+var incorrectWord3;
 
-	// time
-	var timeLeft;
-	var startingTime = 5;
-	var timeText = "";
+// screen text
+var mainText;
+var TRtext;
+var TLtext;
+var BRtext;
+var BLtext;
 
-	// timer
-	var timer;
 
-	// Text styles
-	var smallStyle = {font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-	var medStyle = {font: "bold 45px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-	var bigStyle = {font: "bold 100px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+// score
+var score = 0;
+var scoreText = "";
+
+// lives
+var startingLives = 3;
+var livesLeft;
+var livesBox;
+
+// time
+var timeLeft;
+var startingTime = 5;
+var timeText = "";
+
+// timer
+var timer;
+
+// Text styles
+var smallStyle = {font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+var medStyle = {font: "bold 45px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+var bigStyle = {font: "bold 100px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
 
 
 window.onload = function()
 {
 	// Create game
 	var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
-	$.get('http://localhost:1337/student/testdata',function(data) {
-		console.log(data);
+
+			// Add states
+			game.state.add('loading', LoadingScreen);
+			game.state.add('start', StartScreen);
+			game.state.add('rules', RulesScreen);
+			game.state.add('play', PlayScreen);
+			game.state.add('finish', FinishScreen);
+
+			// Show loading screen
+			game.state.start('loading');
+
+	$.get('http://localhost:1337/student/te2stdata', function(data)
+	{
+		console.log("GET" + data);
 		groupWords = data;
-		// Add states
-		game.state.add('start', StartScreen);
-		game.state.add('rules', RulesScreen);
-		game.state.add('play', PlayScreen);
-		game.state.add('finish', FinishScreen);
+
+			// Multiple inputs
+		// groupWords = data[0];
+		// startingCoins = data[1];
+
+
+	});
+
+	$.post('http://localhost:1337/student/s2avedata',{gamename:'The Sheep Game'}, function(data)
+	{
+		// Log returned data
+		console.log("POST" + data);
 
 		// Start game
 		game.state.start('start');
 	});
-	$.post('http://localhost:1337/student/savedata',{gamename:'The Sheep Game'},function(data) {
-		console.log(data);
-	});
-
 };
