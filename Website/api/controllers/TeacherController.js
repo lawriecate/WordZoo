88a7e5e09998b7c2690aebbb3b7686bc65935c90
Lawrie Cate <lawrie@catefamily.co.uk>
2018-02-01 15:08:52 +0000

    added teacher controller

--- Website/api/controllers/TeacherController.js
+++ Website/api/controllers/TeacherController.js
@@ -7,7 +7,13 @@
 
 module.exports = {
 		home: function(req,res) {
-			 
+			schools = School.findOne({id:req.user.teaches_at[0].id}).exec(function(err,school) {
+			 if (err) {
+				 return res.serverError(err);
+			 }
+			 return res.view('teacher/home.ejs', {'title':'Hello', school: school});
+
+		 });
 		},
 		viewClass: function(req,res) {
 		},
