80239c3f5ec74afb4a975003d8ecd5f00f88bd76
Lawrie Cate <lawrie@catefamily.co.uk>
2018-02-18 15:17:15 +0000

    Play model added

---
+++ Website/api/models/Play.js
@@ -0,0 +1,22 @@
+/**
+ * Play.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    game_id: {
+      type: 'integer',
+      required: true
+    },
+    pupil_id: {
+      type: 'integer',
+      required: true
+    }
+  },
+
+
+};
