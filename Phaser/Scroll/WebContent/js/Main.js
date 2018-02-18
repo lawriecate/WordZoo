9da8bb62b15d4f8ddb417ca13039fa6510c414db
Oliver <32636766+obodinar@users.noreply.github.com>
2018-02-18 17:21:51 +0000

    Scroll

---
+++ Phaser/Scroll/WebContent/js/Main.js
@@ -0,0 +1,45 @@
+window.onload = function() 
+{
+	var game = new Phaser.Game(1052, 288, Phaser.AUTO);
+
+	// Add the States your game has.
+	//game.state.add("Loading", Loading);
+	game.state.add("Level", Level);
+
+
+	// Go to *** LOADING ***
+	game.state.start("Level");
+
+	/*
+	$.get('http://localhost:1337/student/testdata', function(info)
+	{
+		//console.log("GET" + info);
+
+			// Multiple inputs
+		// username = info[0];
+
+
+	}).fail(function() {
+		console.log('i failed');
+	});
+
+	$.post('http://localhost:1337/student/savedata',{gamename:'Character'}, function(info)
+	{
+		// Log info
+		//console.log("POST" + info);
+
+
+
+
+
+		// Wait until level is loaded
+		while(1)
+		{
+			if(levelLoaded)
+			{
+				// Start Level
+				game.state.start("Level");
+			}
+		}
+	});*/
+};
