8afce495b02120b903ea85619046e21d9810152e
rob8479 <psyrcs@nottingham.ac.uk>
2018-03-05 13:29:02 +0000

    Reverted Back

--- Website/assets/stuhome/CustomCharacter/WebContent/assets/canvas/Level.js
+++ Website/assets/stuhome/CustomCharacter/WebContent/assets/canvas/Level.js
@@ -622,10 +622,6 @@ function getLockID(lock)
  */
 function renderCharacter(data)
 {
-	if(data[40] == 2){
-		console.log("Crown did load correctly");
-	}
-
 	// Loop through data
 	for(var i = 0; i < 36; i++)
 	{
@@ -765,8 +761,6 @@ function updateEquipedItem(item)
 			break;
 		}
 	}
-
-
 	updateDB();
 }
 
